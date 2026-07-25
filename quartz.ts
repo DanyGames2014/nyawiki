aff35b9e5c4e00d097c93efdeac10f78609abe6e
Daniel Fildán <fildand@centrum.cz>
2026-07-25 17:44:45 +0200

    If this works it will be a miracle

--- quartz.ts
+++ quartz.ts
@@ -1,4 +1,18 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import * as ExternalPlugin from "./quartz/plugins"
+
+ExternalPlugin.Explorer({
+  // 1. Show same-named files (like NyaLib.md inside NyaLib/) inside the tree
+  filterFn: undefined, // Clears default filter so index/matching files aren't hidden
+
+  // 2. Prevent 404s by making folders without markdown pages unclickable
+  mapFn: (node) => {
+    if (node.isFolder && !node.data) {
+      node.slug = undefined // Prevents Quartz from rendering an <a> tag link
+    }
+    return node
+  },
+})
 
 const config = await loadQuartzConfig()
 export default config
