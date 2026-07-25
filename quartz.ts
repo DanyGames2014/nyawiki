2d32692f6c66a628418e0a74730759df8ed56cd5
Daniel Fildán <fildand@centrum.cz>
2026-07-25 17:52:56 +0200

    Please?

--- quartz.ts
+++ quartz.ts
@@ -1,4 +1,10 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import * as ExternalPlugin from "./quartz/plugins"
+
+// Disables the default filter so NyaLib.md stays visible inside NyaLib/
+ExternalPlugin.plugins["explorer"].Explorer({
+  filterFn: undefined,
+})
 
 const config = await loadQuartzConfig()
 export default config
